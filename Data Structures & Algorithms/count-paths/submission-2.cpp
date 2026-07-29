1588b2cd1ccef26768ad2da7279074f19a57b023
kaushal-K9 <160782884+kaushal-K9@users.noreply.github.com>
2026-07-29 23:36:36 +0530

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        vector<vector<int>> t(m, vector<int>(n));
+
+        t[0][0] = 1;
+
+        //fill 0th row with 1's
+        for (int col = 1; col < n; col++) {
+            t[0][col] = 1;
+        }
+
+        //fill 0th col with 1's
+        for (int row = 1; row < m; row++) {
+            t[row][0] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                t[i][j] = t[i - 1][j] + t[i][j - 1];
+            }
+        }
+
+        return t[m - 1][n - 1];
+    }
+};
